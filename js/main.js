16ca0cb21b272746fcc4f3a58c417193f911c229
Joshua Moore <77459489+joshuamoore5899@users.noreply.github.com>
2022-10-31 14:08:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,73 +1,85 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-
-document.querySelector('#first-button').addEventListener('click', getCocktail);
 let counter = 0;
+document.querySelector('#first-button').addEventListener('click', getCocktail);
+
 
 function getCocktail() {
   const cocktailName = document.querySelector('#first').value;
+    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+      .then(res => res.json())
+      .then(data => {
+        if (document.getElementById('alc').checked) {
+          data = data.drinks.filter((x) => x.strAlcoholic === 'Alcoholic');
+        }
+        else if (document.getElementById('nonalc').checked) {
+          data = data.drinks.filter((x) => x.strAlcoholic === 'Non alcoholic');
+        }
+        else {
+          data = data.drinks.filter((x) => x.strIngredient1 != null);
+        }
+        if (data.length < 1) {
+          document.querySelector('h2').innerText = 'There are no matches. Try to search for something else.'
+          return;
+        }
+        if (counter > data.length - 1) counter = 0;
+        document.querySelector('h2').innerText = data[counter].strDrink;
+        document.querySelector('img').src = data[counter].strDrinkThumb;
+        // document.querySelector('img').style.border = "15px solid turquoise";
 
-  fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktailName)
-    .then(res => res.json())
-    .then(data => {
-      if (counter >= data.drinks.length) counter = 0;
-      console.log(data.drinks)
-      document.querySelector('h2').innerText = data.drinks[counter].strDrink;
-      document.querySelector('img').src = data.drinks[counter].strDrinkThumb;
-      document.querySelector('img').style.border = "15px solid turquoise";
+        let ingredients = "";
 
-      let ingredients = ""
-      if (data.drinks[counter].strIngredient1 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient1 + " ");
-      }
-      if (data.drinks[counter].strIngredient2 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient2 + " ");
-      }
-      if (data.drinks[counter].strIngredient3 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient3 + " ");
-      }
-      if (data.drinks[counter].strIngredient4 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient4 + " ");
-      }
-      if (data.drinks[counter].strIngredient5 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient5 + " ");
-      }
-      if (data.drinks[counter].strIngredient6 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient6 + " ");
-      }
-      if (data.drinks[counter].strIngredient7 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient7 + " ");
-      }
-      if (data.drinks[counter].strIngredient8 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient8 + " ");
-      }
-      if (data.drinks[counter].strIngredient9 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient9 + " ");
-      }
-      if (data.drinks[counter].strIngredient10 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient10 + " ");
-      }
-      if (data.drinks[counter].strIngredient11 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient11 + " ");
-      }
-      if (data.drinks[counter].strIngredient12 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient12 + " ");
-      }
-      if (data.drinks[counter].strIngredient13 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient13 + " ");
-      }
-      if (data.drinks[counter].strIngredient14 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient14 + " ");
-      }
-      if (data.drinks[counter].strIngredient15 != null) {
-        ingredients += ("- " + data.drinks[counter].strIngredient15 + " ");
-      }
+        if (data[counter].strIngredient1 != null) {
+          ingredients += (data[counter].strIngredient1 + " ");
+        }
+        if (data[counter].strIngredient2 != null) {
+          ingredients += ("- " + data[counter].strIngredient2 + " ");
+        }
+        if (data[counter].strIngredient3 != null) {
+          ingredients += ("- " + data[counter].strIngredient3 + " ");
+        }
+        if (data[counter].strIngredient4 != null) {
+          ingredients += ("- " + data[counter].strIngredient4 + " ");
+        }
+        if (data[counter].strIngredient5 != null) {
+          ingredients += ("- " + data[counter].strIngredient5 + " ");
+        }
+        if (data[counter].strIngredient6 != null) {
+          ingredients += ("- " + data[counter].strIngredient6 + " ");
+        }
+        if (data[counter].strIngredient7 != null) {
+          ingredients += ("- " + data[counter].strIngredient7 + " ");
+        }
+        if (data[counter].strIngredient8 != null) {
+          ingredients += ("- " + data[counter].strIngredient8 + " ");
+        }
+        if (data[counter].strIngredient9 != null) {
+          ingredients += ("- " + data[counter].strIngredient9 + " ");
+        }
+        if (data[counter].strIngredient10 != null) {
+          ingredients += ("- " + data[counter].strIngredient10 + " ");
+        }
+        if (data[counter].strIngredient11 != null) {
+          ingredients += ("- " + data[counter].strIngredient11 + " ");
+        }
+        if (data[counter].strIngredient12 != null) {
+          ingredients += ("- " + data[counter].strIngredient12 + " ");
+        }
+        if (data[counter].strIngredient13 != null) {
+          ingredients += ("- " + data[counter].strIngredient13 + " ");
+        }
+        if (data[counter].strIngredient14 != null) {
+          ingredients += ("- " + data[counter].strIngredient14 + " ");
+        }
+        if (data[counter].strIngredient15 != null) {
+          ingredients += ("- " + data[counter].strIngredient15 + " ");
+        }
 
-      document.querySelector('div').innerText = ingredients;
-      document.querySelector('h3').innerText = data.drinks[counter].strInstructions;
+        document.querySelector('#drinkdiv').innerText = ingredients;
+        document.querySelector('h3').innerText = data[counter].strInstructions;
 
-      counter++;
-    })
-    .catch(err => {
-      console.log(`error ${err}`);
-    })
+        counter++;
+      })
+      .catch(err => {
+        console.log(`error ${err}`);
+      })
 }
